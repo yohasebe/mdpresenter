cc67572259931c693d9d0540463ef789bd07ce52
Yoichiro Hasebe <yohasebe@gmail.com>
2020-10-06 12:07:27 +0900

    js

--- docs/yohasebe.js
+++ docs/yohasebe.js
@@ -67,7 +67,7 @@ $(document).ready(function(){
 
       var next_elem_height = 0;
       if($(this).next()){
-        next_elemn_height = $(this).next().height();
+        next_elem_height = $(this).next().height();
       }
 
       var padding = parseInt((windowHeight - (targetImg.height + next_elem_height) / 2)) + "px"
