a7ca2c42e5c16b12ac621d1cc6b4de1f1a8505e9
Yoichiro Hasebe <yohasebe@gmail.com>
2020-12-28 22:11:28 +0900

    ...

--- docs/mdpresenter.js
+++ docs/mdpresenter.js
@@ -395,5 +395,29 @@ $(window).on("load", function(){
     }
   });
 
+  // code to enable drag scroll
+  var clicked = false, clickY;
+  $(document).on({
+    'mousemove': function(e) {
+      clicked && updateScrollPos(e);
+    },
+    'mousedown': function(e) {
+      clicked = true;
+      clickY = e.pageY;
+    },
+    'mouseup': function() {
+      clicked = false;
+      $('html').css('cursor', 'auto');
+    }
+  });
+
+  var updateScrollPos = function(e) {
+    $('html').css('cursor', 'move');
+    $(window).scrollTop($(window).scrollTop() + (clickY - e.pageY));
+  }
+
   toHome();
 });
+
+
+
